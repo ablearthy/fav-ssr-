f722cbd96e3bd2713d289a2ae5f025c87046cbd3
Timur Aminev <ablearthy@gmail.com>
2024-01-30 00:26:58 +0300

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Description";
+
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        devTools = with pkgs; [
+          bashInteractive
+          
+          coqPackages.coq
+          coqPackages.mathcomp
+          coqPackages.equations
+
+          coqPackages.coq-lsp
+        ];
+      in {
+        devShell = pkgs.mkShell ({
+          buildInputs = devTools;
+        });
+      }
+    );
+}
